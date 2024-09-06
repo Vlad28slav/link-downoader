79db4e2d011cd8fb342a564e8990ba4c898cee4a
vladislav95 <noconquer@gmail.com>
2024-09-06 15:32:33 +0200

    files upload to dropbox account

--- ddd.py
+++ ddd.py
@@ -1,13 +1,13 @@
 from typing import Optional
 import dropbox
 import sys
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
 
-ACCESS_TOKEN = "sl.B8XbSn3ocOXC7vFS1P6r8FdAvdPcp-zG-bH5UOlEpQKLBkoJOI7H4oxYy8lc28dOmdtI0LclT9CqH3pmj0sDsUjkTxMCq7qvEbLa0IcKgOsHnacS6l9XG4LX6fZq--sYtl71qgXRbpQT"
+ACCESS_TOKEN = "sl.B8at-3ELLVuv1PCwzsL2vQikKzKYtqNgGKwoUNAT8FkQmcoOTh8I33P42tzeIaHE1WMJmbJv4H4OJwjQy8T3NxzH_sLdhJOfM3GS5Qyd_eb1ybmx3q5pAtqPkPxqVsicMVYBbxM1rWvA"
 LOCALFILE = 'text.txt'
 
 app = FastAPI()
@@ -19,19 +19,16 @@ templates = Jinja2Templates(directory="templates")
 def main(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
-@app.post('/get_file')
-async def get_file(file: UploadFile):
-    print(file.filename)
-    return {"filename": file}
 
 @app.post("/upload_file/")
-async def upload_file(
+async def upload_file(file: UploadFile
 ):
     with dropbox.Dropbox(ACCESS_TOKEN) as dbx:
 
         # Check that the access token is valid
         try:
             dbx.users_get_current_account()
+            LOCALFILE= file.filename
         except AuthError:
             sys.exit("ERROR: Invalid access token; try re-generating an "
                 "access token from the app console on the web.")
